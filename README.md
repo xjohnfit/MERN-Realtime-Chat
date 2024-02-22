225cd101dc926f001a9e3201a2dd70873fffb5b0
xjohnfit <xjohnfitcodes@gmail.com>
2024-02-22 12:06:40 -0500

    README.md

---
+++ README.md
@@ -0,0 +1,18 @@
+# ReactJS + NodeJS + MongoDB Atlas Chat-App
+
+> Start backend: inside root folder run:
+
+`npm run server`
+
+> Start frontend: inside frontend folder run:
+
+`npm run dev`
+
+## Dependencies
+* bcryptjs
+* cookie-parser
+* dotenv
+* express
+* jsonwebtoken
+* mongoose
+* socket.io
--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 const SignUp = () => {
   return (
-    <div>
-      
+    <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+      <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+        <h1 className='text-3xl font-semibold text-center text-gray-300'>
+          SignUp <span className='text-blue-500'>ChatApp</span>
+        </h1>
+
+        <form>
+          
+        </form>
+      </div>
     </div>
   )
 }
