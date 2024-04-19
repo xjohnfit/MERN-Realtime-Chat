d866ff01b6e452f05aa939016fc814838b14a2ae
John Rocha <xjohnfitcodes@gmail.com>
2024-04-18 20:38:06 -0400

    Socket URL Updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
     useEffect(() => {
         if(authUser){
-            const socket = io("http://localhost:5000", {
+            const socket = io("https://realtime-chat-hb7q.onrender.com", {
                 query: {
                     userId: authUser._id,
                 }
